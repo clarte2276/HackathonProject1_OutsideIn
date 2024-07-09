1160ebd88b880248719a333a0ea2d28405d2d700
ssungcoding <tjdgus2532@gmail.com>
2024-07-09 20:39:32 +0900

    채팅 목록 만들기

---
+++ Back-end/chatlist.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const mysql = require("mysql");
+const db_config = require("./config/db_config.json");
+const moment = require("moment");
+const router = express.Router();
+const path = require("path");
+const app = express();
+const session = require("express-session");
+
+// URL을 인코딩하는 코드
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+const pool = mysql.createPool({
+  connectionLimit: 10,
+  host: db_config.host,
+  user: db_config.user,
+  password: db_config.password,
+  database: db_config.database,
+  port: db_config.port,
+  debug: false,
+});
+
+// 게시글 목록 조회 (GET)
+router.get("/process/chat", (req, res) => {
+  pool.query("SELECT nickname FROM users", (error, results) => {
+    if (error) {
+      console.error(error);
+      res.status(500).send("서버 오류");
+    } else {
+      res.json(results); // JSON 형식으로 응답
+    }
+  });
+});
+
+module.exports = router;
