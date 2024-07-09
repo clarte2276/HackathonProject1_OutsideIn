5c4359f4a67e49de4053288783d6904088b2768f
on22y <lucy14lee@gmail.com>
2024-07-10 02:22:03 +0900

    chat 구현은 안 되는데 일단 업데이트해놓음

--- Front-end/src/App.js
+++ Front-end/src/App.js
@@ -1,20 +1,23 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar.js";
-import Header from "./components/Header.js";
-import CheckList from "./components/CheckList.js";
-import CalendarFunc from "./components/CalendarFunc.js";
-import Board from "./components/Board.js";
-import BoardTap from "./components/page/BoardTap.js";
-import MypageTap from "./components/MypageTap.js";
-import LoginTap from "./components/LoginTap.js";
-import Signup from "./components/Signup.js";
-import JoyBoard from "./components/page/JoyBoard.js";
-import SadnessBoard from "./components/page/SadnessBoard.js";
-import AnxietyBoard from "./components/page/AnxietyBoard.js";
-import FearBoard from "./components/page/FearBoard.js";
-import Chat from "./components/page/Chat.js";
+import React, { useEffect } from 'react';
+import io from 'socket.io-client';
+import SocketContext from './components/page/ChatSocket.js';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar.js';
+import Header from './components/Header.js';
+import CheckList from './components/CheckList.js';
+import CalendarFunc from './components/CalendarFunc.js';
+import Board from './components/Board.js';
+import BoardTap from './components/page/BoardTap.js';
+import MypageTap from './components/MypageTap.js';
+import LoginTap from './components/LoginTap.js';
+import Signup from './components/Signup.js';
+import JoyBoard from './components/page/JoyBoard.js';
+import SadnessBoard from './components/page/SadnessBoard.js';
+import AnxietyBoard from './components/page/AnxietyBoard.js';
+import FearBoard from './components/page/FearBoard.js';
+import Chat from './components/page/Chat.js';
+import ChatService from './components/page/ChatService.js';
 
 const Home = () => {
   return (
@@ -36,26 +39,36 @@ const Home = () => {
   );
 };
 
+const socket = io.connect('http://localhost:3000');
+
 function App() {
+  useEffect(() => {
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Navbar />
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/joy" element={<JoyBoard />} />
-          <Route path="/MyPage" element={<MypageTap />} />
-          <Route path="/LoginTap" element={<LoginTap />} />
-          <Route path="/Signup" element={<Signup />} />
-          <Route path="/joy" element={<JoyBoard />} />
-          <Route path="/sadness" element={<SadnessBoard />} />
-          <Route path="/anxiety" element={<AnxietyBoard />} />
-          <Route path="/Fear" element={<FearBoard />} />
-          <Route path="/joy/:page" component={JoyBoard} />
-          <Route path="/process/chat" element={<Chat />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <SocketContext.Provider value={socket}>
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/joy" element={<JoyBoard />} />
+            <Route path="/MyPage" element={<MypageTap />} />
+            <Route path="/LoginTap" element={<LoginTap />} />
+            <Route path="/Signup" element={<Signup />} />
+            <Route path="/joy" element={<JoyBoard />} />
+            <Route path="/sadness" element={<SadnessBoard />} />
+            <Route path="/anxiety" element={<AnxietyBoard />} />
+            <Route path="/Fear" element={<FearBoard />} />
+            <Route path="/joy/:page" component={JoyBoard} />
+            <Route path="/process/chat" element={<Chat />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </SocketContext.Provider>
   );
 }
 
---
+++ Front-end/src/components/page/ChatSocket.js
@@ -0,0 +1,7 @@
+// 소켓과의 연결
+
+import React from 'react';
+
+const SocketContext = React.createContext();
+
+export default SocketContext;
